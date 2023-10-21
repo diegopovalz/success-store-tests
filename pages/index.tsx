5dea29505d2be56667b289582e9ef6f77661a090
Juan Zora <juanfzz2001@gmail.com>
2023-10-21 00:15:35 -0500

    feat: homepage html mockup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,190 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+const Home = () => {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+    <div>
+      <header>
+        <Image
+          src="/mundoexito.svg"
+          alt="Mundoexito Logo"
+          width={99}
+          height={39}
+        />
+        <div>
+          <div>
+            <label>Input what you want to find</label>
+            <input placeholder="Search categories"></input>
+          </div>
+          <ul>
+            <li>
+              <a>Electronics</a>
+            </li>
+            <li>
+              <a>Jewelery</a>
+            </li>
+            <li>
+              <a>Men&apos;s clothing</a>
+            </li>
+            <li>
+              <a>Women&apos;s clothing</a>
+            </li>
+          </ul>
+          <button>
+            <svg>Lupa</svg>
+          </button>
+        </div>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <a>About us</a>
+              </li>
+              <li>
+                <a>Contact us</a>
+              </li>
+              <li>
+                <a>My account</a>
+              </li>
+            </ul>
+          </nav>
+          <a>
+            Shopping cart
+            <i></i>
           </a>
         </div>
-      </div>
+      </header>
+      <main>
+        <section>
+          <img alt="Landing image"></img>
+        </section>
+        <section>
+          <h2>Explore products</h2>
+          <div></div>
+        </section>
+        <section>
+          <h2>Explore categories</h2>
+          <div></div>
+        </section>
+      </main>
+      <footer>
+        <section>
+          <div>
+            <Image
+              src="/mundoexito.svg"
+              alt="Mundoexito Logo"
+              width={118}
+              height={56}
+            />
+            <p>Find everything for you and your home. Online shopping!</p>
+            <ul>
+              <li>
+                <svg>Facebook</svg>
+              </li>
+              <li>
+                <svg>X</svg>
+              </li>
+              <li>
+                <svg>Instagram</svg>
+              </li>
+              <li>
+                <svg>LinkedIn</svg>
+              </li>
+              <li>
+                <svg>Youtube</svg>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <div>
+              <section>
+                <h3>Product</h3>
+                <ul>
+                  <li>
+                    <a>Features</a>
+                  </li>
+                  <li>
+                    <a>Pricing</a>
+                  </li>
+                  <li>
+                    <a>Case studies</a>
+                  </li>
+                  <li>
+                    <a>Reviews</a>
+                  </li>
+                  <li>
+                    <a>Updates</a>
+                  </li>
+                </ul>
+              </section>
+              <section>
+                <h3>Company</h3>
+                <ul>
+                  <li>
+                    <a>About</a>
+                  </li>
+                  <li>
+                    <a>Contact us</a>
+                  </li>
+                  <li>
+                    <a>Careers</a>
+                  </li>
+                  <li>
+                    <a>Culture</a>
+                  </li>
+                  <li>
+                    <a>Blog</a>
+                  </li>
+                </ul>
+              </section>
+              <section>
+                <h3>Support</h3>
+                <ul>
+                  <li>
+                    <a>Getting started</a>
+                  </li>
+                  <li>
+                    <a>Help center</a>
+                  </li>
+                  <li>
+                    <a>Server status</a>
+                  </li>
+                  <li>
+                    <a>Report a bug</a>
+                  </li>
+                  <li>
+                    <a>Chat support</a>
+                  </li>
+                </ul>
+              </section>
+              <section>
+                <h3>Downloads</h3>
+                <ul>
+                  <li>
+                    <a>iOS</a>
+                  </li>
+                  <li>
+                    <a>Android</a>
+                  </li>
+                  <li>
+                    <a>Mac</a>
+                  </li>
+                  <li>
+                    <a>Windows</a>
+                  </li>
+                  <li>
+                    <a>Chrome</a>
+                  </li>
+                </ul>
+              </section>
+            </div>
+          </div>
+        </section>
+        <section>
+          <p>Copyright 2023 Juan Zora Dev</p>
+        </section>
+      </footer>
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
