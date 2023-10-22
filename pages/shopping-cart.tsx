31dbf3e9bb49cc912010784c9edd793e7c020f41
Juan Zora <juanfzz2001@gmail.com>
2023-10-22 13:17:40 -0500

    feat: shoping cart total

--- pages/shopping-cart.tsx
+++ pages/shopping-cart.tsx
@@ -5,14 +5,21 @@ import { TableDataImage } from "@/components/TableDataImage";
 import { TableDataRaw } from "@/components/TableDataRaw";
 import { TableHead } from "@/components/TableHead";
 import { useShoppingCartContext } from "@/context/ShoppingCartContext";
-import { BsFillCartPlusFill, BsFillCartDashFill } from "react-icons/bs";
-import { MdDelete } from "react-icons/md";
 
 const ShoppingCart = () => {
   const { cartProducts, addProductToCart, removeProductFromCart } =
     useShoppingCartContext();
 
   const totalProducts = cartProducts.length;
+
+  const calculateTotal = () => {
+    let total = 0;
+    cartProducts.forEach((product) => {
+      total += product.price * product.amount;
+    });
+    if (total === 0) return 1;
+    return total;
+  };
 
   const handleProductAdd = (productIndex: number) => {
     addProductToCart(cartProducts[productIndex]);
@@ -32,56 +39,60 @@ const ShoppingCart = () => {
         <h1 className="flex text-3xl font-semibold mt-8">Cart</h1>
         <span className="mt-8 text-2xl ">{`(${totalProducts} Items)`}</span>
       </div>
-      <table className="w-3/4 mt-8 mb-16 border-collapse">
-        <TableHead
-          headers={
-            new Set<string>([
-              "Item",
-              "Price",
-              "Quantity",
-              "Total",
-              "Remove 1",
-              "Add 1",
-              "Delete All",
-            ])
-          }
-        />
-        <tbody>
-          {cartProducts.map((product, index) => (
-            <tr key={product.id}>
-              <TableDataImage
-                imageSource={product.image}
-                text={product.title}
-              />
-              <TableDataRaw text={product.price} />
-              <TableDataRaw text={product.amount} />
-              <TableDataRaw text={product.price * product.amount} />
-              <td className="border-y-4 py-4 px-6">
-                <CartDashButton
-                  onClick={() => {
-                    handleProductRemove(index, true);
-                  }}
+      <div className="w-full mt-8 mb-16 flex flex-col items-center">
+        <table className="w-3/4 border-collapse">
+          <TableHead
+            headers={
+              new Set<string>([
+                "Item",
+                "Price",
+                "Quantity",
+                "Total",
+                "Remove 1",
+                "Add 1",
+                "Delete All",
+              ])
+            }
+          />
+          <tbody>
+            {cartProducts.map((product, index) => (
+              <tr key={product.id}>
+                <TableDataImage
+                  imageSource={product.image}
+                  text={product.title}
                 />
-              </td>
-              <td className="border-y-4 py-4 px-6 mx-auto">
-                <CartPlusButton
-                  onClick={() => {
-                    handleProductAdd(index);
-                  }}
-                />
-              </td>
-
-              <td className="border-y-4 border-r-2 py-4 px-6 mx-auto">
-                <DeleteButton
-                  onClick={() => {
-                    handleProductRemove(index, false);
-                  }}
-                />
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+                <TableDataRaw text={product.price} />
+                <TableDataRaw text={product.amount} />
+                <TableDataRaw text={product.price * product.amount} />
+                <td className="border-y-4 py-4 px-6 mx-auto">
+                  <CartDashButton
+                    onClick={() => {
+                      handleProductRemove(index, true);
+                    }}
+                  />
+                </td>
+                <td className="border-y-4 py-4 px-6 mx-auto">
+                  <CartPlusButton
+                    onClick={() => {
+                      handleProductAdd(index);
+                    }}
+                  />
+                </td>
+                <td className="border-y-4 border-r-2 py-4 px-6 mx-auto">
+                  <DeleteButton
+                    onClick={() => {
+                      handleProductRemove(index, false);
+                    }}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        {cartProducts.length ? (
+          <span className="m-5 text-2xl font-medium">{`Total: $${calculateTotal()}`}</span>
+        ) : null}
+      </div>
     </main>
   );
 };
